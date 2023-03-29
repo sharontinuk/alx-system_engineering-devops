2a5c6656e518a35627ff6dcd9841efe1817c9f48
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-03-29 18:10:43 +0100

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,10 @@
-hdgbsn,cvjfg,jdfjdc
+# Creates a file in /tmp
+
+file { 'school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
+}
